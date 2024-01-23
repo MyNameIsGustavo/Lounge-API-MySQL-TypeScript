563f08b0cd5ac2cce730a951f253e8674d7aa64e
MyNameIsGustavo <gustavo.rochamaia@gmail.com>
2024-01-23 16:37:05 -0300

    refac:: controller e service do metodo post de admin

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,17 +1,18 @@
 import express, { Request, Response } from "express";
 import { cadastrarAdmin } from "./admin.service";
+import { admin } from "@prisma/client";
 
 const adminRotas = express.Router();
 
 adminRotas.post('/admin', async (req: Request, res: Response) => {
-    const { user, nome, senha, sobrenome, cpf, codigoAdmin } = req.body;
+    const dadosAdmin: admin = req.body;
 
-    if (!user || !nome || !senha || !sobrenome || !cpf || !codigoAdmin)
+    if (!dadosAdmin.user || !dadosAdmin.nome || !dadosAdmin.senha || !dadosAdmin.sobrenome || !dadosAdmin.cpf || !dadosAdmin.codigoAdmin)
         return res.status(400).send({ message: 'Os parametros [nome, sobrenome, cpf, codigoAdmin] são obrigatorios.' });
 
     try {
-        await cadastrarAdmin(user, nome, senha, sobrenome, cpf, codigoAdmin);
-        return res.status(200).send({ message: 'Admin cadastrado com sucesso!', data: req.body });
+        await cadastrarAdmin(dadosAdmin);
+        return res.status(200).send(req.body);
     } catch (error) {
         console.log(error);
     }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,10 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, admin } from "@prisma/client";
+import { encriptarSenha } from '../utils/bcrypt';
 
 const prisma = new PrismaClient();
 
-export async function cadastrarAdmin(user: string, nome: string, senha: string, sobrenome: string, cpf: string, codigoAdmin: string) {
+export async function cadastrarAdmin(dadosAdmin: admin) {
+    const senhaEncriptada = await encriptarSenha(dadosAdmin.senha);
+    dadosAdmin.senha = senhaEncriptada;
+
     try {
-        return await prisma.admin.create({ data: { user, nome, senha, sobrenome, cpf, codigoAdmin } });
+        return await prisma.admin.create({ data: dadosAdmin });
     } catch (error) {
         console.error('Erro ao cadastrar admin:', error);
     }
